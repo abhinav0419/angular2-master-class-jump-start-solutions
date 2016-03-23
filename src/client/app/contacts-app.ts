fa131606afc68af89d1515cf7c0fd75e5f4e2996
Pascal Precht <pascal.precht@googlemail.com>
2016-03-23 19:47:35 +0100

    step 2: use ContactHeaderComponent as a child component

--- src/client/app/contacts-app.ts
+++ src/client/app/contacts-app.ts
@@ -1,8 +1,13 @@
 import {Component} from 'angular2/core';
+import {ContactHeaderComponent} from './contact-header-component/contact-header-component';
 
 @Component({
   selector: 'contacts-app',
   styleUrls: ['app/contacts-app.css'],
-  template: `<h1>Hello World!</h1>`
+  template: `
+    <contact-header-component></contact-header-component>
+    <h1>Hello World!</h1>
+  `,
+  directives: [ContactHeaderComponent]
 })
 export class ContactsApp {}
