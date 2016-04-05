998f62afc46b7fba3cf8333011fb3bbb52ebc62b
Pascal Precht <pascal.precht@googlemail.com>
2016-04-05 14:33:28 +0200

    step 3: display first contact

--- src/client/app/contacts-app.ts
+++ src/client/app/contacts-app.ts
@@ -1,13 +1,36 @@
 import {Component} from 'angular2/core';
 import {ContactHeaderComponent} from './contact-header-component/contact-header-component';
+import {Contact} from './models/contact';
 
 @Component({
   selector: 'contacts-app',
   styleUrls: ['app/contacts-app.css'],
   template: `
     <contact-header-component></contact-header-component>
-    <h1>Hello World!</h1>
+    <ul class="collection">
+      <li class="collection-item avatar">
+        <img [src]="contact.image" alt="" class="circle">
+        <span class="title">{{contact.name}}</span>
+      </li>
+    </ul>
   `,
   directives: [ContactHeaderComponent]
 })
-export class ContactsApp {}
+export class ContactsApp {
+
+  contact: Contact = {
+    id: 7,
+    name: 'Diana Ellis',
+    email: '',
+    phone: '',
+    birthday: '',
+    website: '',
+    image: '/assets/images/6.jpg',
+    address: {
+      street: '6554 park lane',
+      zip: '43378',
+      city: 'Rush',
+      country: 'United States'
+    }
+  };
+}
